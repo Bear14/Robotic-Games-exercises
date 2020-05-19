6a1bdddcdbfc3a49af2baec0204eb872baf87283
bear14 <bearlord14@gmail.com>
2020-05-19 13:44:34 +0200

    4_1

---
+++ exercise_4/robot.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+class Robot():
+
+	def __init__(self, x, y, phi, width=0, height=0):
+
+		self.pos_x = x
+		self.pos_y = y
+		self.orientation = phi
+		self.trajectory = []
+		
+		self.width = 20
+		self.height = 20
+		
+		
+	def kin_update(self, v, omega, delta_t):
+	
+		self.trajectory.append([self.pos_x, self.pos_y]) # add old position value to trajectory
+	
+		# TODO 4.1) implement kinematics
+		kinematic_array = (np.matrix([[self.pos_x], [self.pos_y], [self.orientation]]) + np.matrix([[np.cos(self.orientation), 0], [np.sin(self.orientation), 0], [0, 1]]) * np.matrix([[v], [omega]]) * delta_t)
+		self.pos_x, self.pos_y, self.orientation = kinematic_array.item(0), kinematic_array.item(1), kinematic_array.item(2)
+
+
+	def alignment_force(self, robots, radius):
+		force = np.zeros(2)
+		# TODO 4.2) implement behavior
+		return force
+
+	
+	def cohesion_force(self, robots, radius): # center of mass
+		force = np.zeros(2)
+		# TODO 4.2) implement behavior
+		return force
+
+	
+	def separation_force(self, robots, radius): # 
+		force = np.zeros(2)
+		# TODO 4.2) implement behavior
+		return force
+
+		
+	def behavior_update(self, robots):
+		total_force = np.zeros(2)
+		# TODO 4.2) play with values
+		c1 = 5
+		c2 = 1
+		c3 = 1
+		
+		align = self.alignment_force(robots, 5)
+		separation = self.separation_force(robots, 5)
+		cohesion = self.cohesion_force(robots, 5)
+		total_force = c1 * align + c2 * separation  + c3 * cohesion
+
+           
+		# Calc force (to absolut value and angle)
+		total_force=np.array([np.sqrt(total_force[0]**2+total_force[1]**2),np.arctan2(total_force[0],total_force[1])])
+		return total_force
+	
+	
+
+
+
+	def edges(self):
+		if self.pos_x > self.width:
+			self.pos_x = 0
+		elif self.pos_x < 0:
+			self.pos_x = self.width
+
+		if self.pos_y > self.height:
+			self.pos_y = 0
+		elif self.pos_y < 0:
+			self.pos_y = self.height
+
+	def show(self):
+		for x in range(len(self.trajectory)):
+			point = self.trajectory[x]
+			plt.plot(point[0], point[1], color='grey', marker='o', markersize=2)
+		plt.arrow(self.pos_x, self.pos_y, 0.2*math.cos(self.orientation), 0.2*math.sin(self.orientation),head_width=0.2, head_length=0.3, fc='k', ec='k')
+		
+	
+		
+		
+		
+	
